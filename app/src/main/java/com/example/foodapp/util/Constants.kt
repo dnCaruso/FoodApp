4f5c630fd289f7897194c0f773dc7e58a7713a4b
Daniel Caruso <danielteixeira33@hotmail.com>
2024-06-21 19:18:01 -0300

    feat: Add util package for common utility functions

---
+++ app/src/main/java/com/example/foodapp/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.foodapp.util
+
+import androidx.compose.ui.unit.dp
+
+object DataConstants {
+    val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+}
+
+object UiConstants {
+    val FLOATING_BUTTON_SIZE = 45.dp
+    val FLOATING_BUTTON_TOP_PADDING = 40.dp
+    val FLOATING_BUTTON_HORIZONTAL_PADDING = 16.dp
+}
