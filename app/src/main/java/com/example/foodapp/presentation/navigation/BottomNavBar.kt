e5ae33745cff993dff43567c0c56dc16d04fa04b
Daniel Caruso <danielteixeira33@hotmail.com>
2024-06-24 11:15:55 -0300

    feat: Implement bottom navigation bar

--- app/src/main/java/com/example/foodapp/presentation/navigation/BottomNavBar.kt
+++ app/src/main/java/com/example/foodapp/presentation/navigation/BottomNavBar.kt
@@ -1,5 +1,8 @@
 package com.example.foodapp.presentation.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
@@ -7,32 +10,67 @@ import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.foodapp.R
+import com.example.foodapp.presentation.theme.HomeScreenDarkRed
 
 val items = listOf(
     BottomNavigationItem(
+        route = Screen.Home.route,
         selectedIcon = Icons.Filled.Home,
         unselectedIcon = Icons.Outlined.Home
     ),
     BottomNavigationItem(
+        route = Screen.Favorites.route,
         selectedIcon = Icons.Filled.Favorite,
         unselectedIcon = Icons.Outlined.FavoriteBorder
     ),
     BottomNavigationItem(
+        route = Screen.Recommended.route,
         selectedIcon = Icons.Filled.ThumbUp,
         unselectedIcon = Icons.Outlined.ThumbUp
     )
 )
 
 @Composable
-fun BottomNavBar(modifier: Modifier = Modifier) {
-    NavigationBar {
-        items.forEachIndexed{ index, item ->
-            NavigationBarItem(selected = , onClick = { /*TODO*/ }, icon = { /*TODO*/ })
+fun BottomNavBar(navController: NavHostController) {
+
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar(modifier = Modifier.height(100.dp), containerColor = HomeScreenDarkRed) {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+                    navController.navigate(item.route)
+                },
+                icon = {
+                    Icon(
+                        imageVector = if (index == selectedItemIndex) {
+                            item.selectedIcon
+                        } else {
+                            item.unselectedIcon
+                        },
+                        contentDescription = "",
+                        tint = if (index == selectedItemIndex) Color.Red else Color.White
+                    )
+                },
+            )
         }
     }
 }
--- app/src/main/java/com/example/foodapp/presentation/navigation/BottomNavigationItem.kt
+++ app/src/main/java/com/example/foodapp/presentation/navigation/BottomNavigationItem.kt
@@ -3,6 +3,7 @@ package com.example.foodapp.presentation.navigation
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavigationItem(
+    val route: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector
 )
